5d437f7bbaee99e16193e45b54ce95e407f85ea1
AnimeshGupta1 <animeshicpc24@gmail.com>
2024-07-15 17:30:54 +0530

    Adjusted the depth

---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,24 @@
+//
+// Created by Animesh Gupta on 15/07/24.
+//
+#ifndef RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#define RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#include "CornerPatternDatabase.h"
+#include "../Model/RubiksCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
